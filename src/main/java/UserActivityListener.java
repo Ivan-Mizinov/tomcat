744e3618bc1948301efeff205492bcbdc3d0ef52
Ivan Mizinov <mizinov.ivan@yandex.ru>
2025-10-17 10:39:35 +0300

    добавлен UserActivityListener, который выводит в консоль время входа и имя пользователя

---
+++ src/main/java/UserActivityListener.java
@@ -0,0 +1,51 @@
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.*;
+import java.util.Date;
+
+@WebListener
+public class UserActivityListener implements HttpSessionListener, HttpSessionAttributeListener {
+    @Override
+    public void attributeAdded(HttpSessionBindingEvent se) {
+        String name = se.getName();
+        Date date = new Date();
+
+        if ("username".equals(name)) {
+            System.out.println("----------------------------------------");
+            System.out.println("Пользователь вошел в систему: " + date);
+            System.out.println("Имя пользователя: " + name);
+        }
+    }
+
+    @Override
+    public void attributeRemoved(HttpSessionBindingEvent se) {
+        String name = se.getName();
+        Date date = new Date();
+
+        if ("username".equals(name)) {
+            System.out.println("----------------------------------------");
+            System.out.println("Пользователь вышел из системы: " + date);
+            System.out.println("Имя пользователя: " + name);
+        }
+    }
+
+    @Override
+    public void attributeReplaced(HttpSessionBindingEvent se) {
+
+    }
+
+    @Override
+    public void sessionCreated(HttpSessionEvent se) {
+        Date date = new Date();
+
+        System.out.println("----------------------------------------");
+        System.out.println("Сессия создана: " + date);
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent se) {
+        Date date = new Date();
+
+        System.out.println("----------------------------------------");
+        System.out.println("Сессия уничтожена: " + date);
+    }
+}
